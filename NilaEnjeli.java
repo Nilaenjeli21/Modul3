6c3582382cc59761dae76061ee989ecfbb8635c1
Nilaenjeli21 <117903203+Nilaenjeli21@users.noreply.github.com>
2022-11-10 21:31:14 +0700

    Add files via upload

---
+++ NilaEnjeli.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul_3;
+
+/**
+ *
+ * @author Administrator
+ */
+public class NilaEnjeli {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java Programming Nila Enjeli !!!");
+    }
+   
+}
---
+++ TheTree.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul_3;
+
+/**
+ *
+ * @author Administrator
+ */
+public class TheTree {
+    public static void main(String[] args){
+        System.out.println("I think I shall never see,\n"+"a poem as lovely as a tree.\n"+
+                "A tree whose hungry mouth is pressed\n"+"Against the Earth's sweet flowing breast.");
+    
+}
+    
+}
